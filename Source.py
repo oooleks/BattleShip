69e07cbdff89e56bb6f00b1313bf27137299297b
oooleks <oooleks@gmail.com>
2015-08-23 12:29:16 -0700

    Minor change
    Minor change

--- Source.py
+++ Source.py
@@ -5,6 +5,7 @@ board = []
 for x in range(5):
     board.append(["O"] * 5)
 
+
 def print_board(board):
     for row in board:
         print(" ".join(row))
@@ -12,8 +13,10 @@ def print_board(board):
 print("Let's play Battleship!")
 print_board(board)
 
+
 def random_row(board):
     return randint(0, len(board) - 1)
+
 
 def random_col(board):
     return randint(0, len(board[0]) - 1)
